8bdbc2acbe8a756534a6597d6c6a3e00894f1a82
Sam Johnston <samuelalbertjohnston@gmail.com>
2018-08-14 11:28:46 -0600

    Toolview added

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -13,17 +13,17 @@ class FAQ extends Component {
 
     render() {
         return (
-            <div class="FAQ_component">
-                <div class="FAQ-header">Frequently Asked Questions</div>
-                <div class="FAQ-questions">
-                    <div class="FAQ-individual-questions">
+            <div className="FAQ_component">
+                <div className="FAQ-header">Frequently Asked Questions</div>
+                <div className="FAQ-questions">
+                    <div className="FAQ-individual-questions">
                         <div>
                             <img src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                         </div>
                         <img src="https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png" alt="see_more"/>
                     </div>
-                    <div class="FAQ-individual-questions">
+                    <div className="FAQ-individual-questions">
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
                         <img src="https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png" alt="see_more"/>
                     </div>
