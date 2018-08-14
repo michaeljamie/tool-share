eaa94234bcec2293c20150922302526b728520ce
ejolsen <ecjolsen@gmail.com>
2018-08-14 12:30:18 -0600

    Updated ToolSearch Componentw (Event Handlers, State)

--- src/components/ToolSearch/ToolSearch.js
+++ src/components/ToolSearch/ToolSearch.js
@@ -9,53 +9,73 @@ class ToolSearch extends Component {
       forSale: false,
       searchTitle: '',
       searchKeywords: [],
+      deliveryOrPickUp: '',
       maxDistance: 0,
       maxPrice: 0
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.setRentalBoolean = this.setRentalBoolean.bind(this);
+    this.setSaleBoolean = this.setSaleBoolean.bind(this);
+
   };
 
   handleChange(e) {
     this.setState( { [e.target.name]: e.target.value })
   };
 
-  setBoolean(e) {
-    this.setState( { [e.target.name]: !this.state })
-  }
+  setRentalBoolean(e) {
+    this.setState( prevState => ({forRent: !prevState.forRent}) )
+  };
+
+  setSaleBoolean(e) {
+    this.setState( prevState => ({forSale: !prevState.forSale}) )
+  };
 
   render() {
+    console.log(this.state)
     return (
       <div className="tool-search-body">
         <div className='search-bar'>
-          <input placeholder="Search By Keyword"/>
+          <input placeholder="Search By Title" onChange={this.handleChange}/>
         </div>
         <div className='search-criteria'>
           <div className='search-criteria-left-box'>
             <div className='search-criteria-minor-box'>
-              For Rent
-              <input type="checkbox" name="forRent" />
-              For Sale
-              <input type="checkbox" name="forSale" />
+            Keyword Search
+              <select className='search-criteria-select' name="searchKeywords" onChange={this.handleChange}>
+                <option value="power-tools">Power Tools</option>
+                <option value="gas-tools">Gas Powered</option>
+                <option value="hand-tools">Hand Tools</option>
+              </select>
             </div>
             <div className='search-criteria-minor-box'>
-              <select className='search-criteria-select' onChange={this.handleChange}>
-                <option value="delivery">Delivery</option>
-                <option value="pick-up">Pick-Up</option>
+              Delivery or Pick-Up
+              <select className='search-criteria-select' name="deliveryOrPickUp" onChange={this.handleChange}>
+                  <option value="pick-up">Pick-Up</option>
+                  <option value="delivery">Delivery</option>
+                  <option value="both">Delivery & Pick-Up</option>
               </select>
             </div>
           </div>
           <div className='search-criteria-right-box'>
             <div className='search-criteria-minor-box'>
-              <div>Max Price</div>
-              <input type="number" name="price" placeholder="Max Price" onChange={this.handleChange}/>
+              <div>
+                For Rent
+                <input type="checkbox" name="forRent" onChange={this.setRentalBoolean}/>
+                For Sale
+                <input type="checkbox" name="forSale" onChange={this.setSaleBoolean}/>
+                <div>Max Price</div>
+                <input type="number" name="maxPrice" placeholder="Max Price" onChange={this.handleChange}/>
+              </div>
             </div>
             <div className='search-criteria-minor-box'>
               <div>Distance</div>
               <div className='search-criteria-distance-radio'>
-                <input type="radio" id="10miles" name="distance" value="email" onChange={this.handleChange}/>
+                <input type="radio" id="10miles" name="maxDistance" value="email" onChange={this.handleChange}/>
                   <label>10mi</label>
-                <input type="radio" id="25miles" name="distance" value="phone" onChange={this.handleChange}/>
+                <input type="radio" id="25miles" name="maxDistance" value="phone" onChange={this.handleChange}/>
                   <label>25mi</label>
-                <input type="radio" id="50miles" name="distance" value="mail" onChange={this.handleChange}/>
+                <input type="radio" id="50miles" name="maxDistance" value="mail" onChange={this.handleChange}/>
                   <label>50mi</label>
               </div>
             </div>
