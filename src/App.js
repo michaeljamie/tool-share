5f99c58c793639f1854fbca415f86f1bdd79cdcf
ejolsen <ecjolsen@gmail.com>
2018-08-14 14:25:09 -0600

    Built Nav Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Routes from './routes';
+import Nav from './components/Nav/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Tool Share (Test)
+        <Nav/>
         {Routes}
       </div>
     );
