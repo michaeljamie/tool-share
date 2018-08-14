a87dde60a30f4da05481c71e0c05e78eed6dfd9f
Sam Johnston <samuelalbertjohnston@gmail.com>
2018-08-14 14:00:14 -0600

    skeleton framework of FAQ component

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -7,8 +7,14 @@ class FAQ extends Component {
 
         this.state = {
             q1Clicked: false,
-            q2Clicked: false
+            q2Clicked: false,
+            q3Clicked: false,
+            q4Clicked: false,
         }
+    }
+
+    dropDownAnswer = (property) => {
+        this.setState({ [property]: !this.state[property]})
     }
 
     render() {
@@ -16,20 +22,30 @@ class FAQ extends Component {
             <div className="FAQ-component">
                 <div className="FAQ-header">Frequently Asked Questions</div>
                 <div className="FAQ-questions">
-                    <div className="FAQ-individual-questions">
-                        <div className="question-paragraph">
-                            <img className="Q" src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
-                            <div className="actual-question">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-                        </div>
-                        <img className="FAQ-down-arrow" src="https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png" alt="see_more"/>
+                    <div className="FAQ-individual-questions" onClick={ () => {this.dropDownAnswer('q1Clicked')} }>
+                        <img className="Q" src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
+                        <div className="actual-question">How does Tool Share Work?</div>
+                        <img className="FAQ-down-arrow" src={this.state.q1Clicked ? "https://www.iconsdb.com/icons/preview/black/arrow-141-xxl.png" : "https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png"} alt="see_more"/>
                     </div>
-                    <div className="FAQ-individual-questions">
-                        <div className="question-paragraph">
-                            <img className="Q"  src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
-                            <div className="actual-question">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
-                        </div>
-                        <img className="FAQ-down-arrow" src="https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png" alt="see_more"/>
+                    <div className={this.state.q1Clicked ? "answer-displayed" : "answer-not-displayed"}>Answer:  You pay people money to use their tools.</div>
+                    <div className="FAQ-individual-questions" onClick={ () => {this.dropDownAnswer('q2Clicked')} }>
+                        <img className="Q" src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
+                        <div className="actual-question">How can I be sure I won't be raped when I pick up tools?</div>
+                        <img className="FAQ-down-arrow" src={this.state.q2Clicked ? "https://www.iconsdb.com/icons/preview/black/arrow-141-xxl.png" : "https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png"} alt="see_more"/>
                     </div>
+                    <div className={this.state.q2Clicked ? "answer-displayed" : "answer-not-displayed"}>Answer:  Honestly there's no guarantee you won't get raped so this is a pointless question.</div>
+                    <div className="FAQ-individual-questions" onClick={ () => {this.dropDownAnswer('q3Clicked')} }>
+                        <img className="Q" src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
+                        <div className="actual-question">What do I do if I suspect the tools I'm renting out are being used for murder?</div>
+                        <img className="FAQ-down-arrow" src={this.state.q3Clicked ? "https://www.iconsdb.com/icons/preview/black/arrow-141-xxl.png" : "https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png"} alt="see_more"/>
+                    </div>
+                    <div className={this.state.q3Clicked ? "answer-displayed" : "answer-not-displayed"}>Answer:  I wouldn't say anything bro, snitches get stitches.</div>
+                    <div className="FAQ-individual-questions" onClick={ () => {this.dropDownAnswer('q4Clicked')} }>
+                        <img className="Q" src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
+                        <div className="actual-question">Why do I need to put in so much personal information?</div>
+                        <img className="FAQ-down-arrow" src={this.state.q4Clicked ? "https://www.iconsdb.com/icons/preview/black/arrow-141-xxl.png" : "https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png"} alt="see_more"/>
+                    </div>
+                    <div className={this.state.q4Clicked ? "answer-displayed" : "answer-not-displayed"}>Answer:  Mark Zuckerberg is paying us to collect as much data about you as we can.</div>
                 </div>
             </div>
         )
