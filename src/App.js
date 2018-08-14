0d6fe42185b72f086ef1fb2a901707c235fb708a
ejolsen <ecjolsen@gmail.com>
2018-08-14 11:00:42 -0600

    Test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 import Routes from './routes';
 
-
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Tool Share
+        Tool Share (Test)
         {Routes}
       </div>
     );
