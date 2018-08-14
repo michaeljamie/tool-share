d87512829fdb1529005e26079223cb164b45db9f
Sam Johnston <samuelalbertjohnston@gmail.com>
2018-08-14 11:53:58 -0600

    updated FAQ

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -13,19 +13,22 @@ class FAQ extends Component {
 
     render() {
         return (
-            <div className="FAQ_component">
+            <div className="FAQ-component">
                 <div className="FAQ-header">Frequently Asked Questions</div>
                 <div className="FAQ-questions">
                     <div className="FAQ-individual-questions">
-                        <div>
-                            <img src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                        <div className="question-paragraph">
+                            <img className="Q" src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
+                            <div className="actual-question">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
                         </div>
-                        <img src="https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png" alt="see_more"/>
+                        <img className="FAQ-down-arrow" src="https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png" alt="see_more"/>
                     </div>
                     <div className="FAQ-individual-questions">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
-                        <img src="https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png" alt="see_more"/>
+                        <div className="question-paragraph">
+                            <img className="Q"  src="https://www.iconsdb.com/icons/preview/black/letter-q-xxl.png" alt="Q"/>
+                            <div className="actual-question">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+                        </div>
+                        <img className="FAQ-down-arrow" src="https://www.iconsdb.com/icons/preview/black/arrow-203-xxl.png" alt="see_more"/>
                     </div>
                 </div>
             </div>
