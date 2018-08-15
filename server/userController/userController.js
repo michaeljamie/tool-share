774da63ab0215076967248f08b3057afa0e12e59
Sam Johnston <samuelalbertjohnston@gmail.com>
2018-08-15 10:29:18 -0600

    server stuff

--- server/userController/userController.js
+++ server/userController/userController.js
@@ -1,7 +1,7 @@
 module.exports = {
     read: ( req, res ) => {
         let { auth_id } = req.session.user
-        req.app.get('db').get_user_info([ autho_id ])
+        req.app.get('db').get_user_info([ auth_id ])
         .then( response => {
             req.session.user = response[0]
             res.sendStatus(200)
