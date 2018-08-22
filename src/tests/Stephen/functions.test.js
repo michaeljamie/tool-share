44bece334922489f7686e97e4f6d978da0666174
Stephen Pace <stephen@Stephens-MacBook-Pro.local>
2018-08-22 11:39:36 -0600

    firebase

--- src/tests/Stephen/functions.test.js
+++ src/tests/Stephen/functions.test.js
@@ -1,6 +1,18 @@
-const fns = require("./functions");
+const fns = require('./functions');
+
+
 
 test('test', ()=>{
     let answer = fns.test();
     expect(answer).toBe('test')
+})
+
+test('user data should exist', ()=>{
+    let answer = fns.getUser();
+    expect(answer).toBeDefined()
+})
+
+test('zipcode should be 5 digits', ()=>{
+    let answer = fns.getZip();
+    expect(answer.length).toBe(5)
 })
