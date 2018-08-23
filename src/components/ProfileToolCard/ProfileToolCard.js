5596ea12d0bcb19829df4fe1314b62cd40cae0d3
seankidwell <dev.seankidwell@gmail.com>
2018-08-23 15:59:15 -0600

    added rent indicators for profile tool cards

--- src/components/ProfileToolCard/ProfileToolCard.js
+++ src/components/ProfileToolCard/ProfileToolCard.js
@@ -1,15 +1,21 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import check from '../../assets/greenCheck.png';
+import x from '../../assets/redX.png';
 
 export default class ProfileToolCard extends Component {
   render() {
     return (
       <div className='profileToolCard-card'>
-          <img src={this.props.toolImg} alt='tool' className='profileToolCard-image'/>
+        <img src={this.props.toolImg} alt='tool' className='profileToolCard-image'/>
+        {this.props.currentlyAvailable?
+        <img src={check} alt='in' className='profileToolCard-ifRented'/>:
+        <img src={x} alt='out' className='profileToolCard-ifRented'/>
+        }
         <div className='profileToolCard-cardTitle'>{this.props.toolName}</div>
         <div className='profileToolCard-cardPrice'>{this.props.toolPrice}/day</div>
         <Link to={`/toolview/${this.props.toolId}`}>
-        <div className='profileToolCard-cardShade'/>
+          <div className='profileToolCard-cardShade'/>
         </Link>
       </div>
     )
