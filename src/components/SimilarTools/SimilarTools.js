82c64e2c5f836738227a2b666f5ace5c7c8e6bb1
ejolsen <ecjolsen@gmail.com>
2018-08-27 09:12:55 -0600

    Committing before pull

--- src/components/SimilarTools/SimilarTools.js
+++ src/components/SimilarTools/SimilarTools.js
@@ -10,34 +10,33 @@ class SimilarTools extends Component {
         };
     };
 
-    componentDidUpdate(prevProps) {
-        if(prevProps.tags !== this.props.tags){
-            this.getToolsWithSameTags()
-        };
-    };
+    // componentDidUpdate(prevProps) {
+    //     if(prevProps.tags !== this.props.tags){
+    //         this.getToolsWithSameTags()
+    //     };
+    // };
 
-    getToolsWithSameTags = () => {
-        axios.get('/api/get_matching_tags').then( res => {
-            this.setState({
-                matchingTagsAndTools: res.data
-            });
-        });
-    };
+    // getToolsWithSameTags = () => {
+    //     let tagsAndID = this.props.tags[0]
+    //     console.log(tagsAndID)
+    //     axios.post('/api/get_matching_tags', tagsAndID).then( res => {
+    //         this.setState({
+    //             matchingTagsAndTools: res.data
+    //         });
+    //     });
+    // };
 
     render() {
-        
-        let tagsAndID = this.props.tags[0]
-        console.log(tagsAndID)
 
-        let toolsWithSameTags = this.state.matchingTagsAndTools.filter( tool => {
-            return (
-                <div>{tool.tool_name}</div>
-            );
-        });
+        // let toolsWithSameTags = this.state.matchingTagsAndTools.map( tool => {
+        //     return (
+        //         <div>{tool.tool_name}</div>
+        //     );
+        // });
 
         return (
            <div>
-               {toolsWithSameTags}
+             
            </div>
         );
     };
